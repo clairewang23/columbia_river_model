6ffdfa667d23199cf1eea0a9390ce6d09f5f5163
Claire Wang <160966720+clairewang23@users.noreply.github.com>
2025-11-23 16:32:59 -0500

    modeling

--- Reservoir.py
+++ Reservoir.py
@@ -26,7 +26,7 @@ class Reservoir:
         self.max_storage = SA * (pool_elev - bottom_elev)
     
     def simulate_fish_passage(self, keep):
-        if keep == 1: # remove dam
+        if keep == 0: # remove dam
             return 1
         return self.fish_pass
     
@@ -65,13 +65,26 @@ class Reservoir:
     
     def simulate_hydropower(self, head, flow, keep):
         inflow = np.minimum(flow, self.pc)
-        P = rho * g * head * eta * self.num_turb * inflow
+        P = rho * g * head * eta * self.num_turb * inflow*0.0283
 
         if keep == 0: # no dam
             P = 0
         # else: yes dam
         P = np.maximum(P, 0) # non-negativity constraint
-        #P = np.minimum((P/1000), self.capacity) # maximum power output is less than rated capacity of turbine
+        P = np.minimum((P/1000), self.capacity) # maximum power output is less than rated capacity of turbine
         energy = (P/1000) * 24 # Watts to kW multiplied by hours in a day to get kWh=
         return energy
     
+    def simulate(self, keep, datetime, outflow, precip):
+        '''
+        Inputs:
+        - keep: if dam is kept, has value of 1. If dam is removed, has value of 0. 
+        - datetime: array of datetime values for each datapoint
+        - outflow: array of reservoir outflow values (cfs)
+        - precip_data: array of precipitation values (in)
+        Return Dataframe containing reservoir simulation data.
+        Dataframe includes columns: datetime, outflow, inflow, storage, hydropower
+        '''
+        simulated_reservoir = pd.DataFrame({'Datetime':datetime,'Outflow (cfs)':outflow, 'Precip (in)':precip})
+        fish_passage = self.simulate_fish_passage(keep)
+        return simulated_reservoir, fish_passage
