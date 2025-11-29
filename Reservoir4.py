53383161e094a7d0db4c2cc117f8a0b88f7e3edd
Claire Wang <160966720+clairewang23@users.noreply.github.com>
2025-11-29 16:49:36 -0500

    alpha and beta for each res

--- Reservoir4.py
+++ Reservoir4.py
@@ -8,25 +8,29 @@ g = 9.81 # gravitational acceleration, 9.81 m/s^2
 
 class Reservoir:
 
-    def __init__(self, SA, num_turb, capacity, tail_elev, pool_elev, bottom_elev, fish_pass, area, pc, spillway_cap):
+    def __init__(self, SA, capacity, tail_elev, pool_elev, bottom_elev, fish_pass, pc, spillway_cap, alfa, beta):
         self.SA = SA # reservoir surface area (sq m)
-        self.num_turb = num_turb # number of turbines at dam
         self.capacity = capacity # generation capacity (kW)
         self.tail_elev = tail_elev*0.3046 # tailwater elevation (ft -> m)
         self.pool_elev = pool_elev*0.3046 # reservoir maximum pooling elevation (ft -> m)
         self.bottom_elev = bottom_elev*0.3046 # reservoir bottom elevation (ft -> m)
         self.fish_pass = fish_pass # fish passage rate
-        self.area = area # watershed area (sq m)
         self.pc = pc # powerhouse capacity (cfs)
-        self.spillway_cap = spillway_cap
+        self.spillway_cap = spillway_cap*0.3046**3 # spillway capacity (cfs -> m^3/s)
         self.max_storage = self.SA * (self.pool_elev - self.bottom_elev) #m^3
+        self.alfa = alfa
+        self.beta = beta
+    
+    def set_params(self, alfa, beta):
+        self.alfa = alfa
+        self.beta = beta
     
     def simulate_fish_passage(self, keep):
         if keep == 0: # remove dam
             return 1
         return self.fish_pass
     
-    def simulation_nat_lake(self, keep, param, h_in, n):
+    def simulation_nat_lake(self, keep, h_in, n):
         """
         Simulate natural lake level, storage, and release trajectories.
         
@@ -40,8 +44,8 @@ class Reservoir:
         """
         # Access parameters by dictionary keys
         S = self.SA      # Lake surface area [m^2]
-        beta = param['beta'] # Storage-discharge parameter
-        alfa = param['alfa'] # Storage-discharge parameter
+        beta = self.beta # Storage-discharge parameter
+        alfa = self.alfa  # Storage-discharge parameter
         h0 = self.tail_elev    # Reference lake level [m]
         h_bottom = self.bottom_elev
         
@@ -85,8 +89,8 @@ class Reservoir:
         - float or array-like: Regulated lake release
         """
         # Natural storage-discharge relationship
-        beta = param['beta']
-        alfa = param['alfa']
+        beta = self.beta
+        alfa = self.alfa
         h0 = self.tail_elev
         
         # Regulated storage-discharge relationship
@@ -107,7 +111,7 @@ class Reservoir:
         
         return r
     
-    def simulation_reg_lake(self, param, h_in, n):
+    def simulation_reg_lake(self, keep, param, h_in, n):
         # Full implementation of simulation_reg_lake
         """
         Simulate regulated lake level, storage, and release trajectories.
@@ -123,10 +127,14 @@ class Reservoir:
         # Lake model parameters
         S = self.SA  # Lake surface area [m^2]
         h_bottom = self.bottom_elev
+        h0 = self.tail_elev
 
         # Integration step and simulation horizon
         H = len(n) - 1              # Simulation horizon [days]
         delta = 60 * 60 * 24        # Integration step [s/day]
+
+        if keep == 0:
+            return np.zeros(len(n)), np.full(len(n), h0), n
 
         # Initialize variables for trajectories
         h = np.full(len(n), np.nan)  # Lake level [m]
@@ -141,35 +149,35 @@ class Reservoir:
         for i in range(H):
             # Compute regulated release using the provided parameters. Clip to ensure no negative storage.
             r[i + 1] = min(self.regulated_release(param, h[i]), s[i]/delta + n[i])
-            # Update storage based on the mass balance
-            s[i + 1] = s[i] + (n[i + 1] - r[i + 1]) * delta
+            # Update storage based on the mass balance. Do not exceed max storage
+            s[i + 1] = min(s[i] + (n[i + 1] - r[i + 1]) * delta, self.max_storage)
             # Compute new lake level
             h[i + 1] = s[i + 1] / S + h_bottom
 
         return s, h, r
 
 
-    def simulate_head(self, storage):
-        #storage given from sim_storage which outputs m^3, SA and elev in meters^2 and m
-        water_height = storage/self.SA + self.bottom_elev # m
-        head = water_height - self.tail_elev #m
+    def simulate_head(self, height):
+        head = height - self.tail_elev #m
         return head #m
     
     def simulate_hydropower(self, head, flow, keep):
-        #flow from function: m^3/day
-        #pc given in cfs
-        inflow = np.minimum(flow, self.pc * 86400*.0283) #m^3/day
+        # flow from function: m^3/s
+        # pc given in cfs
+        inflow = np.minimum(flow, self.pc * .0283) #m^3/s
+
         if keep == 0: # no dam
             P = 0
         elif keep == 1: #yes dam
             #rho: kg/m^3
             #g: m/s^2
             #head: meters
-            #inflow: CALCULATED FROM FUNCTION m^3/day (convert to m^3/s)
+            #inflow: m^3/s
             #(kg*m*m*m^3)/(m^3*s^2*s) = (kg*m^2)/(s^3)
-            P = rho * g * head * eta * inflow/86400 #Watts
+            P = rho * g * head * eta * inflow #Watts
             P = np.maximum(P, 0) # non-negativity constraint
             P = np.minimum(P/1000, self.capacity) # maximum power output is less than rated capacity of turbine, (converted to kW)
+        
         energy = P * 24 # kW multiplied by hours in a day to get kWh
 
         return energy
@@ -180,7 +188,7 @@ class Reservoir:
         avg_annual_hydro = annual_hydro.mean()
         return avg_annual_hydro
     
-    def simulate(self, keep, initial_storage, param, datetime, prev_out, tributary):
+    def simulate(self, keep, initial_height, param, datetime, prev_out, tributary):
         '''
         Inputs:
         - keep: if dam is kept, has value of 1. If dam is removed, has value of 0. 
@@ -192,12 +200,12 @@ class Reservoir:
         '''
 
         # make calculations
-        fish_passage = self.simulate_fish_passage(keep)
+        #fish_passage = self.simulate_fish_passage(keep)
         #outflow = self.simulate_outflow(prev_out, tributary, dstorage) #inputs m^3/day,cfs; outputs m^3/day
 
-        storage, height, outflow = self.simulate_nat_lake(keep, param, initial_storage, prev_out + tributary)
-        #head = self.simulate_head(storage) #inputs m^3, outputs m
-        #hydro = self.simulate_hydropower(head, outflow, keep) #inputs m and m^3/day, outputs kWh
-        #avg_hydro = self.calc_avg_annual_hydro(datetime, hydro) #inputs kWh, outputs kWh/year
+        storage, height, outflow = self.simulation_reg_lake(keep, param, initial_height, prev_out + tributary)
+        head = self.simulate_head(height) #inputs m^3, outputs m
+        hydro = self.simulate_hydropower(head, outflow, keep) #inputs m and m^3/day, outputs kWh
+        avg_hydro = self.calc_avg_annual_hydro(datetime, hydro) #inputs kWh, outputs kWh/year
         
-        return None #outflow, avg_hydro, fish_passage
+        return outflow, avg_hydro, height #, fish_passage
